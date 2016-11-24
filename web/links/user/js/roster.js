61e437731bafc49e4a0a2c073be9a283b7336bb8
dongjianbin <dongjianbin@howzhi.com>
2016-11-24 14:02:50 +0800

    增加花名册的校验

---
+++ web/links/user/js/roster.js
@@ -0,0 +1,36 @@
+$('#roster-form').validate({
+    rules: {
+        'trueName': "required",
+        'gender': "required",
+        'number': "required",
+        'native': "required",
+        'phone': "required",
+        'bornTime': "required",
+        'QQ': "required",
+        'constellation': "required",
+        'hobby': "required",
+        'favoriteMusic': "required",
+        'favoriteMovie': "required",
+        'favoriteSport': "required",
+        'favoriteFood': "required",
+        'favoritePlace': "required",
+        'dream': "required",
+    },
+    messages: {
+        'trueName': "请输入姓名",
+        'gender': "请选择性别",
+        'native': "请输入籍贯",
+        'phone': "请输入手机",
+        'bornTime': "请输入生日",
+        'QQ': "请输入QQ",
+        'constellation': "请输入星座",
+        'hobby': "请输入爱好",
+        'favoriteMusic': "请输入喜欢的音乐",
+        'favoriteMovie': "请输入喜欢的电影",
+        'favoriteSport': "请输入喜欢的运动",
+        'favoriteFood': "请输入喜欢的食物",
+        'favoritePlace': "请输入喜欢的地方",
+        'dream': "请输入梦想",
+    }
+
+});
